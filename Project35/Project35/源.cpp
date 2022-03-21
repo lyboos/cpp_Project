3ae3811e766e98a3bd920bd5ad1775a9c98bbb7b
201250119 <201250119@smail.nju.edu.cn>
2022-03-22 01:32:29 +0800

    average

--- Project35/Project35/源.cpp
+++ Project35/Project35/源.cpp
@@ -1,48 +1,234 @@
 #define  _CRT_SECURE_NO_WARNINGS 
+#pragma once
 #include<iostream>
 #include<limits>
 #include <sstream>
 #include<stdlib.h>
-
+#include<stdio.h>
 using namespace std;
 
-
 int main() {
-    char res[20][60] = { 0 };
-    int n;
-    char str[60];
-    cin >> str >> n;
-    int row = 0,column=0;
-    for (int i = 0; i < strlen(str); i++)
+    char bfcode[1023] = { 0 }, paper[1023] = {0};
+    int pointer = 0;
+    int stack[233], stp = 0;
+    cin.getline(bfcode,1023);
+    for (int i = 0; bfcode[i]!='\0'; i++)
     {
-        if (i % 4 == 0) {
-            if(row)
+        switch (bfcode[i]) {
+        case '+':
+            paper[pointer]++;
+            break;
+        case '-':
+            paper[pointer]--;
+            break;
+        case '>':
+            pointer++;
+            break;
+        case '<':
+            pointer ? pointer-- : pointer;
+            break;
+        case ',':
+            scanf("%c", &paper[pointer]);
+            break;
+        case '.':
+            printf("%c", paper[pointer]);
+            break;
+        case '[':
+            if (paper[pointer] != 0) {
+                stack[stp] = i; stp++;
+            }
+            break;
+        case ']':
+            if (paper[pointer] != '\0') i = stack[stp - 1];
+            else {
+                stack[stp - 1] = 0;
+                stp--;
+            }
+            break;
+        default:break;
         }
-        res[row][column] = str[i];
-        res[row][column+1] = ' ';
-        if (row <= n - 2) row++;
-        else row = 0;
     }
     return 0;
 }
 
 //int main() {
-//    int n = 0,m=0;
-//    cin >> n>>m;
+//    char res[20][120] = { 0 };
+//    int n;
+//    char str[60];
+//    cin >> str >> n;
+//    int row = 0,column=0;
+//    for (int i = 0; i < strlen(str); i++)
+//    {
+//        int index = i % (2 * n - 2);
+//        bool flag = true;
+//        if (index < n) flag = false;
+//        index = index < n ? index : 2 * n - 2 - index;
+//        char temp[] = {str[i],' ','\0'};
+//        strcat(res[index], temp);
+//        if(flag)
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (i != index) {
+//                strcat(res[i], "  ");
+//            }
+//        }
+//    }
+//    for (int i = 0; i < n; i++)
+//    {
+//        cout << res[i] << endl;
+//    }
+//    return 0;
+//}
+//int capacity = 0;
+//int num = 0;
+//void add(int** arr, char* order) {
+//    int* arry = *arr;
+//    if (*arr == NULL) {
+//        arry =(int *)malloc(sizeof(int) * 10);
+//        capacity = 10;
+//        *arr = arry;
+//    }
+//    else if(capacity==num){
+//        int n= capacity + (capacity >> 1);
+//        int* temp = NULL;
+//        temp=*arr;
+//        arry = (int*)malloc(sizeof(int)*n);
+//        for (int i = 0; i < n; i++)
+//        {
+//            *(arry + i) = *(temp+i);
+//        }
+//        capacity = capacity + (capacity >> 1);
+//        *arr = arry;
+//    }
+//    order += 4;
+//    int n = atoi(order);
+//    *(arry + num) = n;
+//    num++;
+//    *arr = arry;
+//}
+//
+//void remove(int** arr, char order[51]) {
+//    int* arry = *arr;
+//    order += 6;
+//    int n = atoi(order);
+//    int i = 0;
+//    for (i = 0; i < num; i++)
+//    {
+//        if (*(arry + i) == n) {
+//            break;
+//        }
+//    }
+//    int* temp= (int*)malloc(sizeof(int) * capacity);
+//    if (i < num) {
+//        int j = 0;
+//        for (j = 0; j < i; j++)
+//        {
+//            *(temp + j) = *(arry + j);
+//        }
+//        for (int k = i+1; k < num; k++)
+//        {
+//            *(temp + j + k - i-1) = *(arry+k);
+//        }
+//        num--;
+//    }
+//}
+//
+//void get(int** arr, char order[51]) {
+//    order += 4;
+//    int n = atoi(order);
+//    if (n >= capacity || n >= num || n <0)
+//    {
+//        cout << -1 << endl;
+//        return;
+//    }
+//    int* arry = *arr;
+//    cout << *(arry + n) << endl;
+//}
+//
+//
+//void getsize(int** arr) {
+//    if (*arr == NULL) {
+//        cout << 0 << endl;
+//    }
+//    else {
+//        cout << num << endl;
+//    }
+//}
+//
+//void getcapa(int** arr) {
+//    if (*arr == NULL) {
+//        cout << 0 << endl;
+//    }
+//    else {
+//        cout << capacity << endl;
+//    }
+//}
+//
+//
+//int main() {
+//    char order[20][51] = { 0 };
+//    int n;
+//    int* arr= NULL;
+//    scanf("%d", &n);
+//    cin.get();
+//    for (int i = 0; i < n; i++)
+//    {
+//        cin.getline(order[i], 51);
+//    }
+//    for (int i = 0; i < n; i++)
+//    {
+//        switch (order[i][0])
+//        {
+//        case 'a':
+//            add(&arr, order[i]);
+//            break;
+//        case 'r':
+//            remove(&arr, order[i]);
+//            break;
+//        case 'g':
+//            if (order[i][3] == ' ') get(&arr, order[i]);
+//            else if (order[i][3] == 'S') getsize(&arr);
+//            else if (order[i][3] == 'C') getcapa(&arr);
+//        default:
+//            break;
+//        }
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int n = 0, m = 0;
+//    cin >> n >> m;
 //    int arr[50][50] = { 0 };
+//    int arr2[50][50] = { 0 };
 //    for (int i = 0; i < n; i++)
 //    {
 //        for (int j = 0; j < n; j++) {
-//            cin >> arr[i][j];
+//            scanf("%d",&arr[i][j]);
 //        }
 //    }
-//    for (int i = 0; i < m; i+=90)
+//    for (int k = 0; k < (m/90); k++)
 //    {
 //        for (int i = 0; i < n; i++) {
-//
+//            for (int j = 0; j < n; j++)
+//            {
+//                arr2[j][n - i-1] = arr[i][j];
+//            }
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++)
+//            {
+//                arr[i][j] = arr2[i][j];
+//            }
 //        }
 //    }
-//    
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j < n; j++)
+//            cout << arr2[i][j]<<' ';
+//        cout << '\n';
+//    }
 //    return 0;
 //}
 //int main() {
@@ -153,7 +339,7 @@ int main() {
 //   
 //    return 0;
 //}
-    //string output;
+//    string output;
     /*string input;
     char temp;
     int num = 0, line = 0;
