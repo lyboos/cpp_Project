df83e1a96a2c9dbc17dae8636c20d7fc23cac585
201250119 <201250119@smail.nju.edu.cn>
2022-04-08 00:25:58 +0800

    saasd

--- Project37/Project37/main.cpp
+++ Project37/Project37/main.cpp
@@ -7,6 +7,79 @@
 
 using namespace std;
 
+
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int res = -9999,count=1;
+		for (int i : nums) {
+			if (i == res) count++;
+			else count--;
+			if (count == 0) res = i;
+		}
+		return res;
+	}
+
+	ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+		if (headB == NULL || headA == NULL) return NULL;
+		ListNode* temp1 = NULL;
+		vector<ListNode*> arr1,arr2;
+		while (headB != NULL)
+		{
+			arr1.push_back(headB);
+			headB = headB->next;
+		}
+		while (headA != NULL)
+		{
+			arr2.push_back(headA);
+			headA = headA->next;
+		}
+		for (ListNode* b:arr1)
+		{
+			for (ListNode* a : arr2) {
+				if (a == b) return a;
+			}
+		}
+		return NULL;
+	}
+
+	bool rotateString(string s, string goal) {
+		if (s == goal) return true;
+		int len1 = s.length();
+		int len2 = goal.length();
+		if (len1 != len2) return false;
+		string temp = s;
+		for (int i = 0; i < len1; i++)
+		{
+			temp = temp.substr(1) + temp.at(0);
+			if (temp == goal) return true;
+		}
+		return false;
+	}
+	bool hasCycle(ListNode* head) {
+		if (head->next == NULL)
+			return false;
+		vector<ListNode*> arr;
+		while (head->next!=NULL)
+		{
+			arr.push_back(head);
+			head = head->next;
+			for (ListNode* temp:arr)
+			{
+				if (temp == head->next)
+					return true;
+			}
+		}
+		return false;
+	}
+};
+
 struct teacher {
 	int id;
 	char name[64];
